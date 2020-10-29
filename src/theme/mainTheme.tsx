a87e855a85b6887da3eef2c7718609fcc5b97afc
unknown <przemyslawsaja@gmail.com>
2020-10-29 19:28:35 +0100

    Basic theme colors added

--- src/theme/mainTheme.tsx
+++ src/theme/mainTheme.tsx
@@ -2,6 +2,10 @@ import { ITheme } from '../__types__/ITheme'
 
 export const theme:ITheme = {
   primmary: '#11A8FD',
+  color100: '#FFFFFF',
+  color200: '#A7A7A7', 
+  color300: '#999999',
+  color400: '#707070',
   fontSize: {
       xs: '0.8em',
       s: '1em',
