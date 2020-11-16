8390a85650543935061cc5db1a38fe53dea55ad7
unknown <przemyslawsaja@gmail.com>
2020-11-16 18:14:59 +0100

    added TrainingDetailis template

---
+++ src/templates/TrainingDetailsTemplate/index.tsx
@@ -0,0 +1,24 @@
+import React, {FC} from 'react'
+import { TailsWrapper } from './style'
+import TrainingDetailsTile from '../../components/molecules/TrainingDetailsTile'
+
+interface ITrainingDetailsTemplate {
+  children: React.ReactNode;
+  trainingId: string;
+}
+const TrainingDetailsTemplate:FC<ITrainingDetailsTemplate> = ({children, trainingId}) => {
+  return (
+    <>
+      <TailsWrapper>
+        <TrainingDetailsTile path={`/trainings/${trainingId}/exercises`} rotation={135} icon="fas fa-dumbbell"/>
+        <TrainingDetailsTile path={`/trainings/${trainingId}/brakes`} icon="far fa-clock" />
+        <TrainingDetailsTile path={`/trainings/${trainingId}/rest`} icon= "fas fa-battery-three-quarters" />
+        <TrainingDetailsTile path={`/trainings/${trainingId}/exercises`} icon= "fas fa-bed" />
+      </TailsWrapper>
+      {children}
+    </> 
+
+  )
+}
+
+export default TrainingDetailsTemplate;
