e946e6ab0902a49aaf655caf6bd5f55684f665db
Karol <KarolKaczorowski@interia.pl>
2020-11-27 23:55:42 +0100

    added logo imitation

---
+++ src/components/molecules/Logo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Div } from './styles';
+
+const Logo = () => {
+  return (
+    <Div>
+      logo
+    </Div>
+  )
+}
+
+export default Logo;
---
+++ src/components/molecules/Logo/styles.ts
@@ -0,0 +1,16 @@
+import styled, { css } from 'styled-components';
+
+export const Div = styled.div`
+  width: 280px;
+  height: 100px;
+  text-align: center;
+  vertical-align: middle;
+  line-height: 100px;
+  font-size: 20px;
+  text-transform: uppercase;
+  margin: 50px 0;
+  ${({ theme }) => css`
+    color: ${theme.colors.primmary.color1};
+    border: 2px solid ${theme.colors.primmary.color1};
+  `}
+`;
