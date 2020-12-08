d2ef4a2594b3adddb528193a7dc083844cd3ef70
Karol <KarolKaczorowski@interia.pl>
2020-12-08 21:13:12 +0100

    added trainingNotes view

--- src/views/TrainingNotes.tsx
+++ src/views/TrainingNotes.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import TrainingDetailsTemplate from '../templates/TrainingDetailsTemplate'
 import MainTemplate from '../templates/MainTemplate'
+import TrainingNote from '../components/molecules/TrainingNote';
+import { Data } from '../ExampleData';
 import { RoutePaths } from '../routes'
 import { useParams } from 'react-router-dom';
+import H2 from '../components/atoms/H2';
+import { TrainingNotesWrapper } from './styles/TrainingNotesStyle';
+
+const getTrainingNotes = (trainingId: string) => {
+  const chosenTraining = Data.UserTrainings.find(({ id }) => id === trainingId);
+  const result = chosenTraining ? 
+      chosenTraining.notes.map((note, i) => <TrainingNote { ...note } key={ i } />)
+    : <H2>No notes</H2>
+
+  return result;
+}
 
 const TrainingNotes = () => {
   const { trainingId } = useParams<{ trainingId: string}>();
-  
+
   return (
     <MainTemplate routePath={RoutePaths.TRAINING_NOTES} buttons={{leftBtn: "Back", rightBtn: "Edit"}}>
       <TrainingDetailsTemplate trainingId={trainingId}>
-        User notes
+        <TrainingNotesWrapper>
+        {
+          getTrainingNotes(trainingId)
+        }
+        </TrainingNotesWrapper>
       </TrainingDetailsTemplate>
     </MainTemplate>
   )
