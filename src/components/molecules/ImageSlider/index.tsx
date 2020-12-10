feb6c74f95a6c80ef0597486997c4445dfecb47c
Karol <KarolKaczorowski@interia.pl>
2020-12-10 23:17:53 +0100

    added ImageSlider component

---
+++ src/components/molecules/ImageSlider/index.tsx
@@ -0,0 +1,36 @@
+import React, { FC, useState } from 'react';
+import { Container, Img, Dot, DotsContainer, SwitchImageButton } from './styles';
+import Icon from '../../atoms/Icon';
+import { theme } from '../../../theme/mainTheme';
+
+const ImageSlider: FC<{ images: string[], }> = ({ images }) => {
+
+  const [currentImageIndex, setCurrentImageIndex] = useState<number>(0);
+
+  return (
+    <Container>
+      { currentImageIndex < images.length - 1 && 
+        <SwitchImageButton style={{ right: '0' }} onClick={ () => setCurrentImageIndex(state => state + 1) }>
+          <Icon size={ 1.6 } className="fas fa-chevron-right" color={ theme.colors.secondary.color3 } />
+        </SwitchImageButton> 
+      }
+      { currentImageIndex > 0 && 
+        <SwitchImageButton style={{ left: '0' }} onClick={ () => setCurrentImageIndex(state => state - 1) }>
+            <Icon size={ 1.6 } className="fas fa-chevron-left" color={ theme.colors.secondary.color3 } />
+        </SwitchImageButton> 
+      }
+      <Img src={ images[currentImageIndex] } alt="" />
+      <DotsContainer>
+        {
+          images.map((_, i) => 
+            <Dot key={ i } 
+              activated={ currentImageIndex === i } 
+              onClick={ () => { setCurrentImageIndex(i) } }
+          />)
+        }
+      </DotsContainer>
+    </Container>
+  )
+} 
+
+export default ImageSlider;
